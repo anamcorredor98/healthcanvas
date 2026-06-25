3a4171e72a1838c39d9c1f1148c9ac348fd2a11d
Ana María Corredor <anamariacorredor98@gmail.com>
2026-06-25 18:48:13 -0500

    ajustes del menú

--- js/main.js
+++ js/main.js
@@ -11,12 +11,14 @@ const navLinks = document.getElementById('navLinks');
 if (navToggle) {
   navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    navToggle.classList.toggle('active');
   });
 
   // Cerrar menú al hacer clic en un link
   navLinks.querySelectorAll('a').forEach(link => {
     link.addEventListener('click', () => {
       navLinks.classList.remove('active');
+      navToggle.classList.remove('active');
     });
   });
 }
